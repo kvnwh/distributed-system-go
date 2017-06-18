d1c6eb4f7be5ca09337b887ed913fe11b8fb37e3
Kevin Huang <kvnhuang10@gmail.com>
2017-06-17 23:40:10 -0400

    added client

--- src/main.go
+++ src/main.go
@@ -6,10 +6,38 @@ import (
 	"log"
 
 	"github.com/streadway/amqp"
+
+	"time"
 )
 
 func main() {
-	server()
+	go client()
+	go server()
+
+	// keep go routine alive
+	var a string
+	fmt.Scanln(&a)
+}
+
+func client() {
+	conn, ch, q := getQueue()
+	// close at the end
+	defer conn.Close()
+	// close after other functions are done
+	defer ch.Close()
+
+	msgs, err := ch.Consume(q.Name, // queue
+		"",    // consumer,
+		true,  // autoAck
+		false, //exclusive
+		false, //nolocal,
+		false, // nowait
+		nil)   // args amqp.Table
+	failOnError(err, "failed to register a consumer")
+
+	for msg := range msgs {
+		log.Printf("Receive message with body: %s", msg.Body)
+	}
 }
 
 func server() {
@@ -17,16 +45,19 @@ func server() {
 	defer conn.Close()
 	defer ch.Close()
 
-	msg := amqp.Publishing{
-		ContentType: "text/plain",
-		Body:        []byte("Hello From Kevin"),
+	for {
+		msg := amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello From Kevin at " + time.Now().Format("03:04:05PM")),
+		}
+		ch.Publish("", q.Name, false, false, msg)
+		// sleep for .5 seconds
+		time.Sleep(500 * time.Millisecond)
 	}
-
-	ch.Publish("", q.Name, false, false, msg)
 }
 
 func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
-	conn, err := amqp.Dial("amqp:guest@localhost:15672")
+	conn, err := amqp.Dial("amqp:guest@localhost:5672")
 	failOnError(err, "Failed to connect to rabbitmq")
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
@@ -37,7 +68,7 @@ func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatal("%s: %s", msg, err)
+		log.Fatal("%s : %s", msg, err)
 		// app will kill itself
 		panic(fmt.Sprint("%s: %s", msg, err))
 	}
